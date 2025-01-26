9da9e8febac4931f53dbe8dc7e7b25490e73a7a3
parvvaresh <parvvaresh@gmail.com>
2025-01-26 15:20:24 +0330

    add semantic_accuracy

---
+++ text/Sentiment_analysis/Independent_Data_Quality_Characteristics/csv/accuracy/semantic_accuracy.py
@@ -0,0 +1,13 @@
+def semantic_accuracy(df, text_column='text', label_column='label'):
+    """
+    Calculate the semantic accuracy of labels in the dataset.
+    
+    Parameters:
+    - df: DataFrame containing the data.
+    - text_column: Name of the column containing the text.
+    - label_column: Name of the column containing the label.
+    
+    Returns:
+    - Semantic accuracy as a value between 0 and 1.
+    """
+    pass
