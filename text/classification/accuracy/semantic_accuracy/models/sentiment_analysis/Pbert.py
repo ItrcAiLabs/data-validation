4195209f473febd64413356e3fd905ecdcc83b97
parvvaresh <parvvaresh@gmail.com>
2025-02-15 08:17:18 +0330

    add bert models for semantic acc

--- text/classification/accuracy/semantic_accuracy/models/sentiment_analysis/Pbert.py
+++
@@ -1,9 +0,0 @@
-"""
-    This code is based on the Parse BERT models developed by Hooshvare Lab.
-
-    For more information visit:
-    URL: https://huggingface.co/HooshvareLab
-"""
-class Pbert:
-    def __init__(self) -> None:
-        pass
