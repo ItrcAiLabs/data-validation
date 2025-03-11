3d89e963a058f579915c9177f3174e87f7ea0e18
parvvaresh <parvvaresh@gmail.com>
2025-03-11 08:52:34 +0330

    del un nec parameter

--- evaluation_license_plate_data/evaluation_license_plate_data.py
+++ evaluation_license_plate_data/evaluation_license_plate_data.py
@@ -8,7 +8,6 @@ def evaluation_license_plate_data(
     image_folder: str,
     threshold_days: float,
     features: list,
-    completeness_required_fields: list,
     expected_counts: dict,
     completeness_field_xpaths: dict,
     required_metadata: list,
@@ -70,7 +69,7 @@ def evaluation_license_plate_data(
           - Currentness Evaluation.
     """
     # Run Completeness Evaluation.
-    comp_json_str = completeness(xml_folder, features, completeness_required_fields, expected_counts, completeness_field_xpaths)
+    comp_json_str = completeness(xml_folder, features, expected_counts, completeness_field_xpaths)
     comp_result = json.loads(comp_json_str)
     
     # Run Accuracy Evaluation.
@@ -93,103 +92,92 @@ def evaluation_license_plate_data(
     return json.dumps(overall_result, ensure_ascii=False, indent=4)
 
 # --- Example Usage ---
-# if __name__ == "__main__":
-#     xml_folder = "/home/reza/Desktop/data-validation/evaluation_license_plate_data/assets/xml"
-#     image_folder = "/home/reza/Desktop/data-validation/evaluation_license_plate_data/assets/img"
-#     threshold_days = 30  # A file is current if it is 30 days old or less.
+if __name__ == "__main__":
+    xml_folder = "/home/reza/Desktop/data-validation/evaluation_license_plate_data/assets/xml"
+    image_folder = "/home/reza/Desktop/data-validation/evaluation_license_plate_data/assets/img"
+    threshold_days = 30  # A file is current if it is 30 days old or less.
     
-#     # Parameters for Completeness Evaluation.
-#     features = [
-#         "LicensePlate/RegistrationPrefix",
-#         "LicensePlate/SeriesLetter",
-#         "LicensePlate/RegistrationNumber",
-#         "LicensePlate/ProvinceCode",
-#         "CarModel",
-#         "CarColor",
-#         "LicensePlateCoordinates/X",
-#         "LicensePlateCoordinates/Y",
-#         "LicensePlateCoordinates/Width",
-#         "LicensePlateCoordinates/Height",
-#         "CarCoordinates/X",
-#         "CarCoordinates/Y",
-#         "CarCoordinates/Width",
-#         "CarCoordinates/Height"
-#     ]
-#     completeness_required_fields = [
-#         "LicensePlate/RegistrationPrefix",
-#         "LicensePlate/SeriesLetter",
-#         "LicensePlate/RegistrationNumber",
-#         "LicensePlate/ProvinceCode",
-#         "CarModel",
-#         "CarColor",
-#         "LicensePlateCoordinates/X",
-#         "LicensePlateCoordinates/Y",
-#         "LicensePlateCoordinates/Width",
-#         "LicensePlateCoordinates/Height",
-#         "CarCoordinates/X",
-#         "CarCoordinates/Y",
-#         "CarCoordinates/Width",
-#         "CarCoordinates/Height"
-#     ]
-#     expected_counts = {
-#         "CarColor": {"brown": 3, "purple": 2, "white": 5},
-#         "CarModel": {"Peugeot-405": 10, "Samand": 8}
-#     }
-#     completeness_field_xpaths = {
-#         "CarColor": "CarColor",
-#         "CarModel": "CarModel",
-#         "RegistrationPrefix": "LicensePlate/RegistrationPrefix"
-#     }
+    xml_config = {
+        "registration_prefix": "LicensePlate/RegistrationPrefix",
+        "series_letter": "LicensePlate/SeriesLetter",
+        "registration_number": "LicensePlate/RegistrationNumber",
+        "province_code": "LicensePlate/ProvinceCode",
+        "car_model": "CarModel",
+        "car_color": "CarColor",
+        "license_plate_coordinates_x" : "LicensePlateCoordinates/X",
+        "license_plate_coordinates_y" : "LicensePlateCoordinates/Y" ,
+        "license_plate_coordinates_width" : "LicensePlateCoordinates/Width",
+        "license_plate_coordinates_height" : "LicensePlateCoordinates/Height",
+        "car_coordinates_x" : "CarCoordinates/X",
+        "car_coordinates_y" : "CarCoordinates/Y",
+        "car_coordinates_width" : "CarCoordinates/Width",
+        "car_coordinates_height" : "CarCoordinates/Height"
+        }
+    # Parameters for Completeness Evaluation.
+    features = list(xml_config.values())
+    expected_counts = {
+        "CarColor": {"brown": 3, "purple": 2, "white": 5},
+        "CarModel": {"Peugeot-405": 10, "Samand": 8}
+    }
+    completeness_field_xpaths = {key: xml_config[key] for key in xml_config if key in {
+                                                                                            "CarModel", 
+                                                                                            "CarColor"    
+                                                                                }}
     
-#     # Parameters for Accuracy Evaluation.
-#     required_metadata = ["width", "height", "format", "location", "date"]
-#     accuracy_required_fields = {
-#         "registration_prefix": "LicensePlate/RegistrationPrefix",
-#         "series_letter": "LicensePlate/SeriesLetter",
-#         "registration_number": "LicensePlate/RegistrationNumber",
-#         "province_code": "LicensePlate/ProvinceCode",
-#         "car_model": "CarModel",
-#         "car_color": "CarColor",
-#         "license_plate_coordinates": "LicensePlateCoordinates",
-#         "car_coordinates": "CarCoordinates"
-#     }
-#     allowed_file_types = ["png"]
-#     dimension_range = {
-#         "min_width": 800,
-#         "max_width": 1920,
-#         "min_height": 600,
-#         "max_height": 1080
-#     }
-#     file_size_range = {
-#         "min_size": 1024,
-#         "max_size": 5000000
-#     }
-#     xpaths_syntactic = {
-#         "registration_prefix": "LicensePlate/RegistrationPrefix",
-#         "series_letter": "LicensePlate/SeriesLetter",
-#         "registration_number": "LicensePlate/RegistrationNumber",
-#         "province_code": "LicensePlate/ProvinceCode"
-#     }
+    # Parameters for Accuracy Evaluation.
+    required_metadata = ["width", "height", "format", "location", "date"]
+    accuracy_required_fields= {key: xml_config[key] for key in xml_config if key in {
+                                                                                        "registration_prefix", 
+                                                                                        "series_letter", 
+                                                                                        "registration_number", 
+                                                                                        "province_code", 
+                                                                                        "car_model", 
+                                                                                        "car_color", 
+                                                                                        "license_plate_coordinates", 
+                                                                                        "car_coordinates"
+                                                                                    }}
+    allowed_file_types = ["png"]
+    dimension_range = {
+        "min_width": 800,
+        "max_width": 1920,
+        "min_height": 600,
+        "max_height": 1080
+    }
+    file_size_range = {
+        "min_size": 1024,
+        "max_size": 5000000
+    }
     
-#     # Parameter for Currentness Evaluation.
-#     currentness_field_xpaths = {"CarModel": "CarModel", "CarColor": "CarColor"}
+    xpaths_syntactic = {key: xml_config[key] for key in xml_config if key in {
+                                                                                    "registration_prefix",
+                                                                                    "series_letter",
+                                                                                    "registration_number",
+                                                                                    "province_code"
+                                                                                }}
     
-#     # Run the overall evaluation.
-#     report = evaluation_license_plate_data(
-#         xml_folder,
-#         image_folder,
-#         threshold_days,
-#         features,
-#         completeness_required_fields,
-#         expected_counts,
-#         completeness_field_xpaths,
-#         required_metadata,
-#         accuracy_required_fields,
-#         allowed_file_types,
-#         dimension_range,
-#         file_size_range,
-#         xpaths_syntactic,
-#         currentness_field_xpaths
-#     )
+
     
-#     print(report)
+    # Parameter for Currentness Evaluation.
+    currentness_field_xpaths = {key: xml_config[key] for key in xml_config if key in {
+                                                                                            "CarModel", 
+                                                                                            "CarColor"    
+                                                                                }}
+    
+    # Run the overall evaluation.
+    report = evaluation_license_plate_data(
+        xml_folder,
+        image_folder,
+        threshold_days,
+        features,
+        expected_counts,
+        completeness_field_xpaths,
+        required_metadata,
+        accuracy_required_fields,
+        allowed_file_types,
+        dimension_range,
+        file_size_range,
+        xpaths_syntactic,
+        currentness_field_xpaths
+    )
+    
+    print(report)
