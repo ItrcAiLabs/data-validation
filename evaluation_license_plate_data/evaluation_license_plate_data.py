5a7cd76fad8cb8be88e0ea5af82cac36a20335eb
parvvaresh <parvvaresh@gmail.com>
2025-03-14 12:21:53 +0330

    clean code :: del un nec parameter

--- evaluation_license_plate_data/evaluation_license_plate_data.py
+++ evaluation_license_plate_data/evaluation_license_plate_data.py
@@ -6,17 +6,13 @@ import json
 def evaluation_license_plate_data(
     xml_folder: str,
     image_folder: str,
+    xml_config : dict,
     threshold_days: float,
-    features: list,
     expected_counts: dict,
-    completeness_field_xpaths: dict,
     required_metadata: list,
-    accuracy_required_fields: dict,
     allowed_file_types: list,
     dimension_range: dict,
     file_size_range: dict,
-    xpaths_syntactic: dict,
-    currentness_field_xpaths: dict = None
 ) -> str:
     """
     Runs the overall evaluation process for license plate data by combining:
@@ -68,18 +64,19 @@ def evaluation_license_plate_data(
           - Accuracy Evaluation, and
           - Currentness Evaluation.
     """
+
+
     # Run Completeness Evaluation.
-    comp_json_str = completeness(xml_folder, features, expected_counts, completeness_field_xpaths)
+    comp_json_str = completeness(xml_folder, xml_config, expected_counts)
     comp_result = json.loads(comp_json_str)
     
     # Run Accuracy Evaluation.
-    acc_json_str = accuracy(image_folder, xml_folder, required_metadata, accuracy_required_fields,
-                            allowed_file_types, dimension_range, file_size_range, xpaths_syntactic)
+    acc_json_str = accuracy(image_folder, xml_folder, xml_config , required_metadata, allowed_file_types, dimension_range, file_size_range)
     acc_result = json.loads(acc_json_str)
     
     # For Currentness Evaluation, use the same folder as image_folder.
     photo_folder = image_folder
-    curr_json_str = Currentness(xml_folder, photo_folder, threshold_days, currentness_field_xpaths)
+    curr_json_str = Currentness(xml_folder, photo_folder, threshold_days, xml_config)
     curr_result = json.loads(curr_json_str)
     
     # Combine all results.
@@ -114,28 +111,16 @@ if __name__ == "__main__":
         "car_coordinates_height" : "CarCoordinates/Height"
         }
     # Parameters for Completeness Evaluation.
-    features = list(xml_config.values())
+
     expected_counts = {
         "CarColor": {"brown": 3, "purple": 2, "white": 5},
         "CarModel": {"Peugeot-405": 10, "Samand": 8}
     }
-    completeness_field_xpaths = {key: xml_config[key] for key in xml_config if key in {
-                                                                                            "CarModel", 
-                                                                                            "CarColor"    
-                                                                                }}
+
     
     # Parameters for Accuracy Evaluation.
     required_metadata = ["width", "height", "format", "location", "date"]
-    accuracy_required_fields= {key: xml_config[key] for key in xml_config if key in {
-                                                                                        "registration_prefix", 
-                                                                                        "series_letter", 
-                                                                                        "registration_number", 
-                                                                                        "province_code", 
-                                                                                        "car_model", 
-                                                                                        "car_color", 
-                                                                                        "license_plate_coordinates", 
-                                                                                        "car_coordinates"
-                                                                                    }}
+
     allowed_file_types = ["png"]
     dimension_range = {
         "min_width": 800,
@@ -147,37 +132,19 @@ if __name__ == "__main__":
         "min_size": 1024,
         "max_size": 5000000
     }
-    
-    xpaths_syntactic = {key: xml_config[key] for key in xml_config if key in {
-                                                                                    "registration_prefix",
-                                                                                    "series_letter",
-                                                                                    "registration_number",
-                                                                                    "province_code"
-                                                                                }}
-    
-
-    
-    # Parameter for Currentness Evaluation.
-    currentness_field_xpaths = {key: xml_config[key] for key in xml_config if key in {
-                                                                                            "CarModel", 
-                                                                                            "CarColor"    
-                                                                                }}
-    
     # Run the overall evaluation.
     report = evaluation_license_plate_data(
-        xml_folder,
-        image_folder,
-        threshold_days,
-        features,
-        expected_counts,
-        completeness_field_xpaths,
-        required_metadata,
-        accuracy_required_fields,
-        allowed_file_types,
-        dimension_range,
-        file_size_range,
-        xpaths_syntactic,
-        currentness_field_xpaths
+                xml_folder,
+                image_folder,
+                xml_config,
+                threshold_days,
+                expected_counts,
+                required_metadata,
+                allowed_file_types,
+                dimension_range,
+                file_size_range,
     )
-    
+    with open("report.json", "w") as json_file:
+        json.dump(report, json_file, indent=4)  # indent=4 makes the output nicely formatted
+
     print(report)
